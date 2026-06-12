97e5f3dbee04b8866f7c3c070b81ab0f9c249b84
bhanuka <hppbsiri@gmail.com>
2026-06-12 07:27:19 +0530

    dwn-3

--- src/sections/hero.jsx
+++ src/sections/hero.jsx
@@ -76,7 +76,7 @@ const Hero = () => {
                 </Button>
               </a>
               <a href="/public/Bhanuka-03.pdf" target="_blank">
-                <button class="relative flex gap-2 focus:outline-none border border-primary px-5 py-2 text-lg rounded-full text-primary-foreground hover:bg-muted bg-muted/75 ">
+                <button className="relative flex gap-2 focus:outline-none border border-primary px-5 py-2 text-lg rounded-full text-primary-foreground hover:bg-muted bg-muted/75 ">
                   <Download />
                   CV Download
                 </button>
